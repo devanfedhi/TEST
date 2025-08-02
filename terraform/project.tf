a4d7502e60d882d98668f23efbaf89e2abff3f08
devanfedhi <devanfedhi1@gmail.com>
2025-08-03 07:16:32 +1000

    test destroy

--- terraform/project.tf
+++ terraform/project.tf
@@ -1,5 +1,5 @@
 resource "digitalocean_project_resources" "project_bbs_resources" {
-  project = data.digitalocean_project.bbs_project.id
+  project = data.digitalocean_project.project_bbs.id
   resources = [
     digitalocean_droplet.droplet_bbs_game_server.urn,
     digitalocean_reserved_ip.droplet_bbs_game_server_ip.urn,
